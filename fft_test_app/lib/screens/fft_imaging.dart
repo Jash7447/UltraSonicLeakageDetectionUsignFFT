c08e3b6a26d2aca092a2174ab0dafe992a8fde87
Harsh Bhagat <93080554+BhagatHarsh@users.noreply.github.com>
2023-11-21 15:38:15 +0530

    update: Removed unnecessary CSV reading components

--- fft_test_app/lib/screens/fft_imaging.dart
+++ fft_test_app/lib/screens/fft_imaging.dart
@@ -2,8 +2,6 @@ import 'dart:math';
 import 'package:camera/camera.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
-import 'package:csv/csv.dart';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:fftea/fftea.dart';
 import 'dart:typed_data';
 
@@ -15,34 +13,8 @@ class FftImaging extends StatefulWidget {
 }
 
 class _FftImagingState extends State<FftImaging> {
-  Future<List<List<dynamic>>>? _csvData;
   List<double> data = [];
   List<FlSpot> spots = [];
-  Future<List<List<dynamic>>> readCSV() async {
-    String timeCSVData =
-        await rootBundle.loadString("assets/sine_wave_time.csv");
-    String amplitudeCSVData =
-        await rootBundle.loadString("assets/sine_wave_amplitude.csv");
-    List<List<dynamic>> timeCSVTable =
-        const CsvToListConverter().convert(timeCSVData);
-    List<List<dynamic>> amplitudeCSVTable =
-        const CsvToListConverter().convert(amplitudeCSVData);
-    List<List<dynamic>> dataList = [];
-    for (int i = 0; i < timeCSVTable.length; i++) {
-      double time = double.parse(timeCSVTable[i][0].toString());
-      double amplitude = double.parse(amplitudeCSVTable[i][0].toString());
-      dataList.add([time, amplitude]);
-    }
-
-    return dataList;
-  }
-
-  Future<List<dynamic>> mockRead() async {
-    String csvData = await rootBundle.loadString('assets/sine_wave_time.csv');
-    List<dynamic> csvTable = const CsvToListConverter().convert(csvData);
-    print(csvTable[1]);
-    return csvTable;
-  }
 
   List<CameraDescription>? cameras; //list out the camera available
   CameraController? controller; //controller for camera
@@ -110,166 +82,12 @@ class _FftImagingState extends State<FftImaging> {
                 decoration: const BoxDecoration(
                     border: Border.symmetric(
                         horizontal: BorderSide(color: Colors.black, width: 2))),
-                child: FutureBuilder<List<List<dynamic>>>(
-                  future: readCSV(),
-                  builder: (BuildContext context,
-                      AsyncSnapshot<List<List<dynamic>>> snapshot) {
+                child: Builder(
+                  builder: (context) {
                     // if (snapshot.connectionState == ConnectionState.done) {
-
-                    List<double> amplitude = [
-                      0,
-                      0.296794316,
-                      0.583752504,
-                      0.851560576,
-                      1.091917487,
-                      1.297965208,
-                      1.464632442,
-                      1.58887173,
-                      1.669776282,
-                      1.708570333,
-                      1.708474634,
-                      1.674456407,
-                      1.612880174,
-                      1.531081948,
-                      1.436893912,
-                      1.33814964,
-                      1.242200942,
-                      1.155476523,
-                      1.083109810,
-                      1.028658808,
-                      0.993934873,
-                      0.978950292,
-                      0.981986984,
-                      0.999780848,
-                      1.027808949,
-                      1.060660172,
-                      1.092464651,
-                      1.117353554,
-                      1.129918864,
-                      1.125642807,
-                      1.101268497,
-                      1.055087120,
-                      0.987122271,
-                      0.899198641,
-                      0.794889589,
-                      0.679345896,
-                      0.559015604,
-                      0.441271827,
-                      0.333971384,
-                      0.244971626,
-                      0.181635632,
-                      0.150356855,
-                      0.15613329,
-                      0.202218275,
-                      0.289870406,
-                      0.418219,
-                      0.584254415,
-                      0.782944841,
-                      1.007473368,
-                      1.249581663,
-                      1.5,
-                      1.748938018,
-                      1.986606817,
-                      2.203741053,
-                      2.392089689,
-                      2.544846021,
-                      2.656991346,
-                      2.725532011,
-                      2.749616193,
-                      2.730524202,
-                      2.671533917,
-                      2.577670681,
-                      2.455358074,
-                      2.311992050,
-                      2.155465553,
-                      1.993673676,
-                      1.834030451,
-                      1.683027440,
-                      1.545861506,
-                      1.426154603,
-                      1.325782486,
-                      1.244822230,
-                      1.181620864,
-                      1.132979655,
-                      1.094441231,
-                      1.060660172,
-                      1.025832369,
-                      0.984154748,
-                      0.930284985,
-                      0.859770869,
-                      0.769420884,
-                      0.657591325,
-                      0.524370575,
-                      0.371647724,
-                      0.203060080,
-                      0.023821859,
-                      -0.159556037,
-                      -0.339638276,
-                      -0.508506516,
-                      -0.658242647,
-                      -0.781423651,
-                      -0.871597015,
-                      -0.923706621,
-                      -0.934442006,
-                      -0.902488498,
-                      -0.828661813,
-                      -0.715917787,
-                      -0.569235636,
-                      -0.395380945,
-                      -0.202562039,
-                      -7.96E-16,
-                      0.202562039,
-                      0.395380945,
-                      0.569235636,
-                      0.715917787,
-                      0.828661813,
-                      0.902488498,
-                      0.934442006,
-                      0.923706621,
-                      0.871597015,
-                      0.781423651,
-                      0.658242647,
-                      0.508506516,
-                      0.339638276,
-                      0.159556037,
-                      -0.023821859,
-                      -0.203060080,
-                      -0.371647724,
-                      -0.524370575,
-                      -0.657591325,
-                      -0.769420884,
-                      -0.859770869,
-                      -0.930284985,
-                      -0.984154748,
-                      -1.025832369,
-                      -1.060660172,
-                      -1.094441231,
-                      -1.132979655,
-                      -1.181620864,
-                      -1.244822230,
-                      -1.325782486,
-                      -1.426154603,
-                      -1.545861506,
-                      -1.683027440,
-                      -1.834030451,
-                      -1.993673676,
-                      -2.155465553,
-                      -2.311992050,
-                      -2.455358074,
-                      -2.577670681,
-                      -2.671533917,
-                      -2.730524202,
-                      -2.749616193,
-                      -2.725532011,
-                      -2.656991346,
-                      -2.544846021,
-                      -2.392089689
-                    ];
-
-                    List<String> lines = generateRandomData(100000).split('\n');
+                    List<String> lines = generateRandomData(1000).split('\n');
                     List<double> numbers = [];
                     // debugPrint(lines.toString());
-
                     for (String line in lines) {
                       List<String> strings = line.trim().split(RegExp(r'\s+'));
                       for (String s in strings) {
